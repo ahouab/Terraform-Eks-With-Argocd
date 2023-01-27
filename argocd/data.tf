47758655043559d246124a15f9370ac6ffe080e4
Calvine Otieno <35563516+NYARAS@users.noreply.github.com>
2023-01-27 18:43:15 +0200

    chore(terraform): add backend config samples for argocd and eks (#20)
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- argocd/data.tf
+++ argocd/data.tf
@@ -1,9 +1,10 @@
+// get the remote state data for eks
 data "terraform_remote_state" "eks" {
   backend = "s3"
 
   config = {
     bucket               = "devops-demo.tfstate"
-    key                  = "argoinfra.json"
+    key                  = "argocdinfra.json"
     region               = "eu-west-1"
     workspace_key_prefix = "environment"
     dynamodb_table       = "devops-demo.tfstate.lock"
