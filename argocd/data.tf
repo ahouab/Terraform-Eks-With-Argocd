1128130a0ea3a9e935407233d88bcf933b8562fd
Calvine Otieno <35563516+NYARAS@users.noreply.github.com>
2023-01-27 15:22:31 +0200

    cleanup(argocd): move providers for argocd to own file (#16)
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>
    
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- argocd/data.tf
+++ argocd/data.tf
@@ -9,33 +9,3 @@ data "terraform_remote_state" "eks" {
     dynamodb_table       = "devops-demo.tfstate.lock"
   }
 }
-
-
-# get EKS authentication for being able to manage k8s objects from terraform
-provider "kubernetes" {
-  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", "${data.terraform_remote_state.eks.outputs.cluster_name}"]
-    command     = "aws"
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-    exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", "${data.terraform_remote_state.eks.outputs.cluster_name}"]
-    command     = "aws"
-  }
-  }
-}
-
-# data "aws_acm_certificate" "arn" {
-#   name = data.terraform_remote_state.eks.outputs.aws_acm_certificate_arn
-# }
-
-# args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.aws_region]
